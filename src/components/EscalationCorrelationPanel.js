db55fdc82a31dbc027cb50a88e0b49f1e7d183f0
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 15:34:58 +0700

    feat(src/components/EscalationCorrelationPanel.js): auto update

---
+++ src/components/EscalationCorrelationPanel.js
@@ -0,0 +1,7 @@
+import { CorrelationPanel } from './CorrelationPanel';
+import { t } from '@/services/i18n';
+export class EscalationCorrelationPanel extends CorrelationPanel {
+    constructor() {
+        super('escalation-correlation', 'Escalation Monitor', 'escalation', t('components.escalationCorrelation.infoTooltip'));
+    }
+}
