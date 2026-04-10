84650938bdf522bcc41b762e2d79a5594053c23f
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:34 +0700

    refactor(src/components/CascadePanel.ts): auto update

--- src/components/CascadePanel.ts
+++ src/components/CascadePanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class CascadePanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'cascade', title: 'Cascade' });
   }
 }
