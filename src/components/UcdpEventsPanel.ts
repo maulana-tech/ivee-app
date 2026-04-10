c45aa78019e4bf9c382008b56d69f82b4e655cbc
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:39 +0700

    refactor(src/components/UcdpEventsPanel.ts): auto update

--- src/components/UcdpEventsPanel.ts
+++ src/components/UcdpEventsPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class UcdpEventsPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'ucdp-events', title: 'UcdpEvents' });
   }
 }
