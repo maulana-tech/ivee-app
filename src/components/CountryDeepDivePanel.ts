745effa73f223b59c41e2882ece4fe86ddd46d64
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:34 +0700

    refactor(src/components/CountryDeepDivePanel.ts): auto update

--- src/components/CountryDeepDivePanel.ts
+++ src/components/CountryDeepDivePanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class CountryDeepDivePanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'country-deep-dive', title: 'CountryDeepDive' });
   }
 }
