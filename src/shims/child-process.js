1d80c585c6f0cc0f9bf52863c9ddabfa07648194
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 19:45:59 +0700

    delete(src/shims/child-process.js): auto update

--- src/shims/child-process.js
+++
@@ -1,8 +0,0 @@
-/**
- * Browser shim for Node's `child_process` module.
- * Some transitive dependencies reference it even in browser bundles.
- */
-export function spawn() {
-    throw new Error('child_process.spawn is not available in browser environments.');
-}
-export default { spawn };
