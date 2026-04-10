f7d9a30efff1a861ebe5169c694ff2d59d29f0da
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:37 +0700

    refactor(src/components/MilitaryCorrelationPanel.ts): auto update

--- src/components/MilitaryCorrelationPanel.ts
+++ src/components/MilitaryCorrelationPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class MilitaryCorrelationPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'military-correlation', title: 'MilitaryCorrelation' });
   }
 }
