e80bb965bbaa53452584a3c80c0c5976b5ed3b9a
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:34 +0700

    refactor(src/components/ChatAnalystPanel.ts): auto update

--- src/components/ChatAnalystPanel.ts
+++ src/components/ChatAnalystPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class ChatAnalystPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'chat-analyst', title: 'ChatAnalyst' });
   }
 }
