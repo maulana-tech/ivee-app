29ee5bf36201ab0b5e6e94bd8f0d87cfa58f417e
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:37 +0700

    refactor(src/components/PopulationExposurePanel.ts): auto update

--- src/components/PopulationExposurePanel.ts
+++ src/components/PopulationExposurePanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class PopulationExposurePanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'population-exposure', title: 'PopulationExposure' });
   }
 }
