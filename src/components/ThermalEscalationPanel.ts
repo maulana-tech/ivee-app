0ac88ab09bcd5f349a288f5c5b0330d68d7b013b
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:39 +0700

    refactor(src/components/ThermalEscalationPanel.ts): auto update

--- src/components/ThermalEscalationPanel.ts
+++ src/components/ThermalEscalationPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class ThermalEscalationPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'thermal-escalation', title: 'ThermalEscalation' });
   }
 }
