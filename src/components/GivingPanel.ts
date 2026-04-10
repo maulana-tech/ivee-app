8ae26dc155219d8f09163d7b1772a67a0e0c51b1
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:36 +0700

    refactor(src/components/GivingPanel.ts): auto update

--- src/components/GivingPanel.ts
+++ src/components/GivingPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class GivingPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'giving', title: 'Giving' });
   }
 }
