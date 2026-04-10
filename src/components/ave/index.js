14b8ecbec4d7b904e5d0481e55297b20db90819c
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 15:35:04 +0700

    feat(src/components/ave/index.js): auto update

---
+++ src/components/ave/index.js
@@ -0,0 +1,5 @@
+export { WhaleAlertPanel } from './WhaleAlertPanel';
+export { SignalsPanel } from './SignalsPanel';
+export { PortfolioPanel } from './PortfolioPanel';
+export { RiskScannerPanel } from './RiskScannerPanel';
+export { TrendingPanel } from './TrendingPanel';
