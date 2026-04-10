34d13f36dfbcfc8f99cb80488d7fc3a21d85a642
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:36 +0700

    refactor(src/components/GulfEconomiesPanel.ts): auto update

--- src/components/GulfEconomiesPanel.ts
+++ src/components/GulfEconomiesPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class GulfEconomiesPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'gulf-economies', title: 'GulfEconomies' });
   }
 }
