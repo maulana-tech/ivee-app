5d296b4f4f6a5cd1a093cbe0fc12e6be8921b18c
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:40 +0700

    refactor(src/components/WorldClockPanel.ts): auto update

--- src/components/WorldClockPanel.ts
+++ src/components/WorldClockPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class WorldClockPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'world-clock', title: 'WorldClock' });
   }
 }
