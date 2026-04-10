c44533c461ee118585215fdb2b2807c03f00f376
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:36 +0700

    refactor(src/components/HeatmapPanel.ts): auto update

--- src/components/HeatmapPanel.ts
+++ src/components/HeatmapPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class HeatmapPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'heatmap', title: 'Heatmap' });
   }
 }
