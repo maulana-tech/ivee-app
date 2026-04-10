dd154193d2f724f6864bcdbf33ccd69addb20bbb
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:34 +0700

    refactor(src/components/CountryBriefPanel.ts): auto update

--- src/components/CountryBriefPanel.ts
+++ src/components/CountryBriefPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class CountryBriefPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'country-brief', title: 'CountryBrief' });
   }
 }
