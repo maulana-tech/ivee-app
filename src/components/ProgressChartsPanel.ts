034762834386134e82597345cd6832ff76af5a11
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:38 +0700

    refactor(src/components/ProgressChartsPanel.ts): auto update

--- src/components/ProgressChartsPanel.ts
+++ src/components/ProgressChartsPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class ProgressChartsPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'progress-charts', title: 'ProgressCharts' });
   }
 }
