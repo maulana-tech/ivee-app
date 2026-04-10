d87e4e0e2d86562241241088828fdf1ee2ce81b3
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:39 +0700

    refactor(src/components/SocialVelocityPanel.ts): auto update

--- src/components/SocialVelocityPanel.ts
+++ src/components/SocialVelocityPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class SocialVelocityPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'social-velocity', title: 'SocialVelocity' });
   }
 }
