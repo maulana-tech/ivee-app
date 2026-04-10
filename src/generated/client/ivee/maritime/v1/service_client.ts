775fa0a19f0f54ee79b7c972a046474a0a2ad90c
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 13:07:10 +0700

    feat(src/generated/client/ivee/maritime/): auto update

---
+++ src/generated/client/ivee/maritime/v1/service_client.ts
@@ -0,0 +1,13 @@
+// @ts-nocheck
+export class MaritimeServiceClient {
+  constructor(baseURL: string, options?: any) {}
+}
+
+export interface NavigationalWarning {
+  id: string;
+  title: string;
+  description: string;
+  coordinates: any;
+  validFrom: number;
+  validTo: number;
+}
