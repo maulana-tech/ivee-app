d7493c74e94cc525afc9988e1a6d1f98e8f3f788
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:35 +0700

    refactor(src/components/FSIPanel.ts): auto update

--- src/components/FSIPanel.ts
+++ src/components/FSIPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class FSIPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'f-s-i', title: 'FSI' });
   }
 }
