7724ded58e8c89ade37301ed73e8dc5a9396d5bb
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 13:07:01 +0700

    refactor(src/components/ave/index.ts): auto update

--- src/components/ave/index.ts
+++ src/components/ave/index.ts
@@ -3,4 +3,3 @@ export { SignalsPanel } from './SignalsPanel';
 export { PortfolioPanel } from './PortfolioPanel';
 export { RiskScannerPanel } from './RiskScannerPanel';
 export { TrendingPanel } from './TrendingPanel';
-export { TradingPanel } from './TradingPanel';
