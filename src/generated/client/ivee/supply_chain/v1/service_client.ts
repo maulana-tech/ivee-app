8226f24356a593c5a202770b4dbac14f3585ce5d
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 13:07:11 +0700

    feat(src/generated/client/ivee/supply_chain/): auto update

---
+++ src/generated/client/ivee/supply_chain/v1/service_client.ts
@@ -0,0 +1,11 @@
+// @ts-nocheck
+export class SupplyChainServiceClient {
+  constructor(baseURL: string, options?: any) {}
+}
+
+export interface TransitDayCount {
+  origin: string;
+  destination: string;
+  transitDays: number;
+  updatedAt: number;
+}
