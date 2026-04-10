5ff1e73e1ce91e6265bab4600d197c9feafcf914
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:34 +0700

    refactor(src/components/CommoditiesPanel.ts): auto update

--- src/components/CommoditiesPanel.ts
+++ src/components/CommoditiesPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class CommoditiesPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'commodities', title: 'Commodities' });
   }
 }
