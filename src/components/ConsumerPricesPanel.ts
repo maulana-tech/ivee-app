59b3139a21c88ed24aba157afd960ffb0a0d00a8
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:34 +0700

    refactor(src/components/ConsumerPricesPanel.ts): auto update

--- src/components/ConsumerPricesPanel.ts
+++ src/components/ConsumerPricesPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class ConsumerPricesPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'consumer-prices', title: 'ConsumerPrices' });
   }
 }
