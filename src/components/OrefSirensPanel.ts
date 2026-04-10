10e86f8a8fa15377a5361fe5d51d99fa010a9076
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:37 +0700

    refactor(src/components/OrefSirensPanel.ts): auto update

--- src/components/OrefSirensPanel.ts
+++ src/components/OrefSirensPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class OrefSirensPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'oref-sirens', title: 'OrefSirens' });
   }
 }
