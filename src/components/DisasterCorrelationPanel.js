784e9aceed53fadc1ad09f4414107f8df083d3b0
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 15:34:57 +0700

    feat(src/components/DisasterCorrelationPanel.js): auto update

---
+++ src/components/DisasterCorrelationPanel.js
@@ -0,0 +1,7 @@
+import { CorrelationPanel } from './CorrelationPanel';
+import { t } from '@/services/i18n';
+export class DisasterCorrelationPanel extends CorrelationPanel {
+    constructor() {
+        super('disaster-correlation', 'Disaster Cascade', 'disaster', t('components.disasterCorrelation.infoTooltip'));
+    }
+}
