b580ab6564f360cb9c12b8f7a9ff9600666ef612
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:37 +0700

    refactor(src/components/PortfolioPanel.ts): auto update

--- src/components/PortfolioPanel.ts
+++ src/components/PortfolioPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class PortfolioPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'portfolio', title: 'Portfolio' });
   }
 }
