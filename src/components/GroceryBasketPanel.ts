54c02ba2fafd71afdd646f7572a4ceeaac59d648
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:36 +0700

    refactor(src/components/GroceryBasketPanel.ts): auto update

--- src/components/GroceryBasketPanel.ts
+++ src/components/GroceryBasketPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class GroceryBasketPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'grocery-basket', title: 'GroceryBasket' });
   }
 }
