ad23b5d130741eb4f09115073ac99943889be1be
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:35 +0700

    refactor(src/components/DiseaseOutbreaksPanel.ts): auto update

--- src/components/DiseaseOutbreaksPanel.ts
+++ src/components/DiseaseOutbreaksPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class DiseaseOutbreaksPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'disease-outbreaks', title: 'DiseaseOutbreaks' });
   }
 }
