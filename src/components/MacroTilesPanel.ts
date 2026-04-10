ec2afce1e78cdff8bb9daa2c323a9ebacfc11e09
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:37 +0700

    refactor(src/components/MacroTilesPanel.ts): auto update

--- src/components/MacroTilesPanel.ts
+++ src/components/MacroTilesPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class MacroTilesPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'macro-tiles', title: 'MacroTiles' });
   }
 }
