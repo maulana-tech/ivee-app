38958d4ca496ac3e3b180f9b18dbf88c00fb8bb0
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:35 +0700

    refactor(src/components/EnergyComplexPanel.ts): auto update

--- src/components/EnergyComplexPanel.ts
+++ src/components/EnergyComplexPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class EnergyComplexPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'energy-complex', title: 'EnergyComplex' });
   }
 }
