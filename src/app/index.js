197986e46a75135c725fcee373ebfee370ea2db1
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 19:45:17 +0700

    delete(src/app/index.js): auto update

--- src/app/index.js
+++
@@ -1,7 +0,0 @@
-export { DesktopUpdater } from './desktop-updater';
-export { CountryIntelManager } from './country-intel';
-export { SearchManager } from './search-manager';
-export { RefreshScheduler } from './refresh-scheduler';
-export { PanelLayoutManager } from './panel-layout';
-export { DataLoaderManager } from './data-loader';
-export { EventHandlerManager } from './event-handlers';
