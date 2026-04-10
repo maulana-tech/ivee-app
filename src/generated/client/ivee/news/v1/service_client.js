90d8c9ac525016883885c29b3d7c14fa9eccc3df
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 15:35:10 +0700

    feat(src/generated/client/ivee/news/v1/service_client.js): auto update

---
+++ src/generated/client/ivee/news/v1/service_client.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+export class NewsServiceClient {
+    constructor(baseURL, options) { }
+}
+export var ThreatLevel;
+(function (ThreatLevel) {
+    ThreatLevel[ThreatLevel["UNKNOWN"] = 0] = "UNKNOWN";
+    ThreatLevel[ThreatLevel["LOW"] = 1] = "LOW";
+    ThreatLevel[ThreatLevel["MODERATE"] = 2] = "MODERATE";
+    ThreatLevel[ThreatLevel["HIGH"] = 3] = "HIGH";
+    ThreatLevel[ThreatLevel["SEVERE"] = 4] = "SEVERE";
+})(ThreatLevel || (ThreatLevel = {}));
