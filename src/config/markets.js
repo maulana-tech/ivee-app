4e28db96f35760ac1f3adde7ee42d731d79ea089
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 19:45:32 +0700

    delete(src/config/markets.js): auto update

--- src/config/markets.js
+++
@@ -1,9 +0,0 @@
-import cryptoConfig from '../../shared/crypto.json';
-import sectorConfig from '../../shared/sectors.json';
-import commodityConfig from '../../shared/commodities.json';
-import stocksConfig from '../../shared/stocks.json';
-export const SECTORS = sectorConfig.sectors;
-export const COMMODITIES = commodityConfig.commodities;
-export const MARKET_SYMBOLS = stocksConfig.symbols;
-export const CRYPTO_IDS = cryptoConfig.ids;
-export const CRYPTO_MAP = cryptoConfig.meta;
