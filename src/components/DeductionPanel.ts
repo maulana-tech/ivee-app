727a8894a1fd45db2b551568c1e3f130e6238d09
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:35 +0700

    refactor(src/components/DeductionPanel.ts): auto update

--- src/components/DeductionPanel.ts
+++ src/components/DeductionPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class DeductionPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'deduction', title: 'Deduction' });
   }
 }
