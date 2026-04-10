c078f5658772a0248bee9df31aec7cb48f81cd30
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:36 +0700

    refactor(src/components/FuelPricesPanel.ts): auto update

--- src/components/FuelPricesPanel.ts
+++ src/components/FuelPricesPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class FuelPricesPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'fuel-prices', title: 'FuelPrices' });
   }
 }
