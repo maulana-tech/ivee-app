fbe45bb5aaae43087dd6a59d9e82f8c042e2aca2
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:36 +0700

    refactor(src/components/GoodThingsDigestPanel.ts): auto update

--- src/components/GoodThingsDigestPanel.ts
+++ src/components/GoodThingsDigestPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class GoodThingsDigestPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'good-things-digest', title: 'GoodThingsDigest' });
   }
 }
