0104943a9761907a0f4000853ecb0f0417a97a5b
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:38 +0700

    refactor(src/components/SanctionsPressurePanel.ts): auto update

--- src/components/SanctionsPressurePanel.ts
+++ src/components/SanctionsPressurePanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class SanctionsPressurePanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'sanctions-pressure', title: 'SanctionsPressure' });
   }
 }
