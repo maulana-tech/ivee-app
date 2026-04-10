ac649904debb7b8f3750e8608b56510c712ff1de
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 15:35:30 +0700

    feat(src/types/index.js): auto update

---
+++ src/types/index.js
@@ -0,0 +1,4 @@
+export const NATURAL_EVENT_CATEGORIES = new Set([
+    'severeStorms', 'wildfires', 'volcanoes', 'earthquakes', 'floods', 'landslides',
+    'drought', 'dustHaze', 'snow', 'tempExtremes', 'seaLakeIce', 'waterColor', 'manmade',
+]);
