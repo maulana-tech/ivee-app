9b552aeccf057fcf2fd365623c6f04024095fac6
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 15:35:00 +0700

    feat(src/components/MilitaryCorrelationPanel.js): auto update

---
+++ src/components/MilitaryCorrelationPanel.js
@@ -0,0 +1,7 @@
+import { CorrelationPanel } from './CorrelationPanel';
+import { t } from '@/services/i18n';
+export class MilitaryCorrelationPanel extends CorrelationPanel {
+    constructor() {
+        super('military-correlation', 'Force Posture', 'military', t('components.militaryCorrelation.infoTooltip'));
+    }
+}
