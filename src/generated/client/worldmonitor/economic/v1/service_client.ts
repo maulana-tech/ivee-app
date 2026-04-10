5e71aa5c6db8ceec0d55d26bfbfc8e807c71a3a8
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 13:07:11 +0700

    feat(src/generated/client/worldmonitor/): auto update

---
+++ src/generated/client/worldmonitor/economic/v1/service_client.ts
@@ -0,0 +1,4 @@
+// @ts-nocheck
+export class EconomicServiceClient {
+  constructor(baseURL: string, options?: any) {}
+}
---
+++ src/generated/client/worldmonitor/news/v1/service_client.ts
@@ -0,0 +1,4 @@
+// @ts-nocheck
+export class NewsServiceClient {
+  constructor(baseURL: string, options?: any) {}
+}
