085cce418d1d2a87a461a141ed1d755bab90c660
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:38 +0700

    refactor(src/components/SecurityAdvisoriesPanel.ts): auto update

--- src/components/SecurityAdvisoriesPanel.ts
+++ src/components/SecurityAdvisoriesPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class SecurityAdvisoriesPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'security-advisories', title: 'SecurityAdvisories' });
   }
 }
