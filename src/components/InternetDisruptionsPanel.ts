37927c34307a9df457466796e94002d0c4a6163d
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:36 +0700

    refactor(src/components/InternetDisruptionsPanel.ts): auto update

--- src/components/InternetDisruptionsPanel.ts
+++ src/components/InternetDisruptionsPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class InternetDisruptionsPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'internet-disruptions', title: 'InternetDisruptions' });
   }
 }
