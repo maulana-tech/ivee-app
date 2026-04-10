3aa0239c00fb45eda1e37f6623502c8e5eb1182c
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 19:45:30 +0700

    delete(src/components/ave/index.js): auto update

--- src/components/ave/index.js
+++
@@ -1,5 +0,0 @@
-export { WhaleAlertPanel } from './WhaleAlertPanel';
-export { SignalsPanel } from './SignalsPanel';
-export { PortfolioPanel } from './PortfolioPanel';
-export { RiskScannerPanel } from './RiskScannerPanel';
-export { TrendingPanel } from './TrendingPanel';
