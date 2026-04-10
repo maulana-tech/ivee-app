d470d5efe1631c28a4c67d4e8f0b7de2e193b148
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:35 +0700

    refactor(src/components/DefensePatentsPanel.ts): auto update

--- src/components/DefensePatentsPanel.ts
+++ src/components/DefensePatentsPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class DefensePatentsPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'defense-patents', title: 'DefensePatents' });
   }
 }
