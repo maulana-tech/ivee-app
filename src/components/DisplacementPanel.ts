e0aa8f2732f9d6fd56c48d77efbaa6468bb251a1
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:35 +0700

    refactor(src/components/DisplacementPanel.ts): auto update

--- src/components/DisplacementPanel.ts
+++ src/components/DisplacementPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class DisplacementPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'displacement', title: 'Displacement' });
   }
 }
