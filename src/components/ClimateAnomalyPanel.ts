0a0301ef0c8ddf002eb1e2e2d4e8bc422760f502
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:34 +0700

    refactor(src/components/ClimateAnomalyPanel.ts): auto update

--- src/components/ClimateAnomalyPanel.ts
+++ src/components/ClimateAnomalyPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class ClimateAnomalyPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'climate-anomaly', title: 'ClimateAnomaly' });
   }
 }
