b3abd9700c9909f35690f480a619bd4a5543fe30
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:37 +0700

    refactor(src/components/LiveNewsPanel.ts): auto update

--- src/components/LiveNewsPanel.ts
+++ src/components/LiveNewsPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class LiveNewsPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'live-news', title: 'LiveNews' });
   }
 }
