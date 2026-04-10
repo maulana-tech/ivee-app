7b38629da946be7a0e88576dd034ab6b1b16c05c
maulana-tech <firdaussyah03@gmail.com>
2026-04-10 23:08:39 +0700

    refactor(src/components/TradePolicyPanel.ts): auto update

--- src/components/TradePolicyPanel.ts
+++ src/components/TradePolicyPanel.ts
@@ -1,6 +1,6 @@
 import { Panel } from './Panel';
-export class STUBPanel extends Panel {
+export class TradePolicyPanel extends Panel {
   constructor() {
-    super({ id: 'stub', title: 'Stub' });
+    super({ id: 'trade-policy', title: 'TradePolicy' });
   }
 }
