a3743a966c25888d5b9b3268816e03803d588cf3
maulana-tech <firdaussyah03@gmail.com>
2026-04-11 16:10:24 +0700

    refactor(src/services/ave/index.ts): auto update

--- src/services/ave/index.ts
+++ src/services/ave/index.ts
@@ -1,4 +1,16 @@
 export * from './client';
 export * from './monitor';
-export * from './signals';
+export {
+  type PriceAlert as MonitoringPriceAlert,
+  type AnomalyEvent,
+  type RiskWarning,
+  getPriceAlerts,
+  addPriceAlert,
+  removePriceAlert,
+  checkPriceAlerts,
+  detectAnomalies,
+  analyzeRisk,
+  scanRisk,
+} from './monitoring';
 export * from './portfolio';
+export * from './signals';
