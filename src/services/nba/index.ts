05b9607a6e540f0904d6783be64ebaa1c6ce974c
FineTune - Full Stack <finetune-ft2@FineTune---Full-Stack-02.local>
2026-05-28 15:51:36 +0700

    refactor(src/services/nba/index.ts): auto update

--- src/services/nba/index.ts
+++ src/services/nba/index.ts
@@ -33,3 +33,28 @@ export {
   type PredictionFactor,
   type StrategyPosition,
 } from './predictions';
+
+export {
+  degasRankService,
+  type DegaPosition,
+  type DegaPerformance,
+  type DegaLeaderboard,
+} from './dega-rank';
+
+export {
+  automationEngine,
+  type AutomationResult,
+  type StrategyConfig,
+  type StrategyType,
+} from './automation-engine';
+
+export {
+  canonBridge,
+  automationResultToTradeSignal,
+} from './canon-bridge';
+
+export {
+  browserExecutionLog,
+  type BrowserLogEntry,
+  type ExecutionLogEntryType,
+} from './browser-execution-log';
